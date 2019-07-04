0829a5b17e859f81bd2de83d67af8ee5ce2c8f08
Cameron Larrabee <cameronlarrabee@yahoo.com>
2019-07-04 07:56:28 -0600

    hello

--- src/App.js
+++ src/App.js
@@ -205,7 +205,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1 className="text-center"><b>SpeakEasy</b></h1>
+        <h1 className="text-center m-5"><b>SpeakEasy</b></h1>
 
         <Search handleFormSubmit={this.handleFormSubmit} handleInputChange={this.handleInputChange} handleSelectChange={this.handleSelectChange} />
 
