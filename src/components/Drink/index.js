e3f8a5e33c51fe9bb498a702d78f9b2006fb14ed
Cameron Larrabee <cameronlarrabee@yahoo.com>
2019-07-06 12:21:51 -0600

    changed result component to Class and added the drink to be displayed to the search by name case, and added models to be displayed when img is clicked on

--- src/components/Drink/index.js
+++ src/components/Drink/index.js
@@ -3,13 +3,47 @@ import React from 'react';
 function Drink(props) {
 
   return (
-    <div id={props.id} >
-      <h4 key={props.index}>{props.title}</h4>
-      <img src={props.img} alt={props.title} />
-      {/* <h5>Ingredients</h5>
-        <p></p>
-        <h5>Instructions</h5>
-        <p></p> */}
+    <div className="col-lg-4 col-md-6 col-sm-12 p-3" id={props.drink.id}>
+      <h4 className="text-center">{props.drink.title}</h4>
+      <div className="d-flex justify-content-center">
+        <img src={props.drink.img} alt={props.drink.title} data-toggle="modal" data-target={`.modal-${props.drink.id}`} />
+
+        <div className={`modal fade modal-${props.drink.id}`} tabIndex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+          <div className="modal-dialog modal-xl">
+            <div className="modal-content">
+              <div className="container">
+                <div className="row">
+                  <div className="col-md-12">
+                    <h1 className="text-center">{props.drink.title}</h1>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-md-4 d-flex justify-content-center">
+                    <img src={props.drink.img} alt={props.drink.title} />
+                  </div>
+
+                  <div className="col-md-4 d-flex justify-content-center">
+                    <ul>
+                      {props.drink.ingredientsArr.map((ingredient) => {
+                        return (
+                          <li>{ingredient}</li>
+                        )
+                      })}
+                    </ul>
+
+                  </div>
+
+                  <div className="col-md-4 d-flex justify-content-center">
+                    <p>{props.drink.instructions}</p>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,21 +1,67 @@
 import React from "react";
 
-function Nav(props) {
-  return (
-    < div >
+class Nav extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isToggleOn: false };
 
-      <ul className="nav nav-pills p-3 ">
-        <li className="nav-item">
-          <a className="nav-link" href="#">Search</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Favorite</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Login</a>
-        </li>
-      </ul>
-    </div >
-  );
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
+  render() {
+
+    return (
+      < div >
+
+        <ul className="nav">
+
+          <li className="nav-item">
+            <button onClick={this.handleClick} type="button" className="btn btn-info float-right" data-toggle="modal" data-target="#exampleModal">
+              {this.state.isToggleOn ? 'Logged In' : 'Sign Out'}
+            </button>
+
+
+            <div className="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+              aria-hidden="true">
+              <div className="modal-dialog" role="document">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div className="modal-body">
+                    <form action="/login" method="post">
+                      <div>
+                        <label>Username:</label>
+                        <input type="text" name="username" />
+                      </div>
+                      <div>
+                        <label>Password:</label>
+                        <input type="password" name="password" />
+                      </div>
+                      <div>
+                        <input type="submit" value="Log In" />
+                      </div>
+                    </form>
+                  </div>
+                  <div className="modal-footer">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div >
+    );
+  }
 }
 export default Nav;
