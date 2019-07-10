2244cb2085260a78a343ea9ecc123996785d500f
cwp132 <44931480+cwp132@users.noreply.github.com>
2019-07-09 21:42:04 -0600

    Revert "added backgrounds and modals"

--- src/components/Drink/index.js
+++ src/components/Drink/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import "./style.css"
 
 function Drink(props) {
 
@@ -12,65 +11,30 @@ function Drink(props) {
         <div className={`modal fade modal-${props.drink.id}`} tabIndex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
           <div className="modal-dialog modal-xl">
             <div className="modal-content">
-              <div className="container p-4">
+              <div className="container">
                 <div className="row">
                   <div className="col-md-12">
                     <h1 className="text-center">{props.drink.title}</h1>
                   </div>
                 </div>
                 <div className="row">
-                  <div className="col-md-3 d-flex justify-content-center">
+                  <div className="col-md-4 d-flex justify-content-center">
                     <img src={props.drink.img} alt={props.drink.title} />
                   </div>
 
-                  <div className="col-md-3">
-                    <h4>Ingredients</h4>
-                    <div className="row">
-                      <ul>
-                        {props.drink.ingredientsArr.map((ingredient, index) => {
-                          if (ingredient === "") {
-                            return (<></>)
-                          } else {
-                            return (
-                              <li key={index}> {ingredient}</li>
-                            )
-                          }
-
-                        })}
-                      </ul>
-                    </div>
-
-                  </div>
-                  <div className="col-md-3">
-                    <div className="row">
-                      <h4>Measurements</h4>
-                    </div>
-                    <div className="row">
-                      <ul id="measureList">
-                        {props.drink.measureArr.map((measure, index) => {
-                          if (measure === " ") {
-                            return (<></>)
-                          } else if (measure === "↵") {
-                            return (<></>)
-                          } else if (measure === "") {
-                            return (<></>)
-                          } else {
-                            return (
-                              <li key={index}>{measure}</li>
-                            )
-                          }
-
-                        })}
-                      </ul>
-                    </div>
+                  <div className="col-md-4 d-flex justify-content-center">
+                    <ul>
+                      {props.drink.ingredientsArr.map((ingredient) => {
+                        return (
+                          <li>{ingredient}</li>
+                        )
+                      })}
+                    </ul>
 
                   </div>
 
-                  <div className="col-md-3 ">
-                    <div className="row">
-                      <h4>Instructions</h4>
-                      <p>{props.drink.instructions}</p>
-                    </div>
+                  <div className="col-md-4 d-flex justify-content-center">
+                    <p>{props.drink.instructions}</p>
                   </div>
 
                 </div>
