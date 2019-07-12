3be3f7514e4f74a52f7d8b2eac2139083281ab03
Cameron Larrabee <cameronlarrabee@yahoo.com>
2019-07-11 21:22:14 -0600

    adjust all filter options display the all the info needed, you do have to click on drinks twice in order to get them to diplay the modal, will work on fixing that later

--- src/utils/API.js
+++ src/utils/API.js
@@ -4,6 +4,9 @@ export default {
   searchByName: function (query) {
     return axios.get("https://www.thecocktaildb.com/api/json/v2/8673533/search.php?s=" + query);
   },
+  modalSearchByName: function (query) {
+    return axios.get("https://www.thecocktaildb.com/api/json/v2/8673533/search.php?s=" + query);
+  },
   searchIngredientByName: function (query) {
     return axios.get("https://www.thecocktaildb.com/api/json/v2/8673533/search.php?i=" + query);
   },
