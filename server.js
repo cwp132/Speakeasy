72ca550ae58b25d4745ff7f7a34b877ea06086cb
cwp132 <44931480+cwp132@users.noreply.github.com>
2019-07-13 10:43:36 -0600

    mongo uri

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.json());
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/CocktailDB", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user1:password1@ds125628.mlab.com:25628/heroku_r702533l", { useNewUrlParser: true });
 
 passport.use(new LocalStrategy(
     function (username, password, done) {
